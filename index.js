704e9f459d8aed1654c0f75da0c5ed9185fc9559
FalnixValencroth <89662789+FalnixValencroth@users.noreply.github.com>
2023-07-30 15:17:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,28 +19,3 @@ function clearDisplay() {
   currentValue = '';
   display.value = '';
 }
-
-  function calculateTotalCost() {
-    const baseCost = 10; // The base cost of the item
-    const discountThreshold = 50; // Quantity threshold for discount
-    const discountPercent = 10; // Percentage discount when quantity exceeds the threshold
-
-    // User input
-    const quantity = parseInt(document.getElementById('quantity').value);
-
-    // Calculate the total cost with discount
-    let totalCost = baseCost * quantity;
-
-    // Apply discount if quantity exceeds the threshold
-    if (quantity >= discountThreshold) {
-      const discountAmount = (totalCost * discountPercent) / 100;
-      totalCost -= discountAmount;
-    }
-
-    // Display the total cost with or without discount
-    const totalCostElement = document.getElementById('totalCost');
-    totalCostElement.textContent = `$${totalCost.toFixed(2)}`;
-  }
-
-  // Add event listener to the "Calculate Total" button
-  document.getElementById('calculateButton').addEventListener('click', calculateTotalCost);
