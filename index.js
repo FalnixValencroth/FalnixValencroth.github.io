5926031557854eec34521a0b81fb8d43a3f44076
FalnixValencroth <89662789+FalnixValencroth@users.noreply.github.com>
2023-07-30 16:22:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ function calculateTotal() {
     const price = parseFloat(document.getElementById('price').value);
     const type = document.getElementById('type').value;
 
-    let baseCost = 10; // Base cost for all types
+    let baseCost = 10;
 
-    // Adjust base cost based on the selected type
+   
     if (type === 'medium') {
         baseCost = 15;
     } else if (type === 'big') {
         baseCost = 20;
     }
 
-    // Calculate the total cost based on the user-provided price
+
     let totalCost = price * quantity;
 
-    // Apply discount based on the quantity of items ordered
+  
     if (quantity <= 10) {
         totalCost *= 0.95; // 5% discount
     } else if (quantity <= 25) {
@@ -24,10 +24,10 @@ function calculateTotal() {
         totalCost *= 0.85; // 15% discount
     }
 
-    // Add the base cost after applying the discount
+
     totalCost += baseCost;
 
-    // Display the total cost with the applied discount
+
     const totalCostElement = document.getElementById('totalCost');
     totalCostElement.textContent = `$${totalCost.toFixed(2)}`;
 }
